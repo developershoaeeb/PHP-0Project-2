02a24726c601caea5b95eef4bfc8f6273097ae55
developershoaeeb <developershoaeeb@gmail.com>
2022-03-27 11:14:04 +0600

    check

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+$NAME = "shoaeeb hossain";
+$number = 5413164;
+$ocapa = "freelanching ";
+
+echo $NAME . ' Occopation ' . $ocapa  . ' Occopation ' . $number;
+
+//echo "$name <br>";
+//echo "My  name is {$NAME}";
+//printf ( "My  name is {$NAME}";)
+//printf("My name is %d <br> my address is %d", $NAME, $name);
+//$myOutPut = sprintf ("My name is %d my address is %d", $NAME, $name);
+//echo $myOutPut; 
+
+define('Project', 'http://shoaeeb.me<br/>', true);
+
+echo ProjectPath;
+
+
+$name34   = "shoaeeb"; //String
+$nameNumber   = "5354959034852098"; //String
+$age    = 8;  //Integer
+$amound = 68.45; //Floot ba Double
+$switch = false; // false; // True or False
+$null   = null; //Null
+
+echo gettype ($age);
+
+$student = array ( 'Rafi', 'Kashem', ' sakib ');
+echo gettype($student);
+?>
+
